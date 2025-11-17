105802d79b38f2e473c275eff8b79abd5f08809c
Axel <axelfraga08@gmail.com>
2025-11-17 15:55:46 -0300

    Refactor PlayerInventory to use WinCondition Tilemap
    Replaced references to mesasTilemap with WinCondition in PlayerInventory.cs for tile interaction logic. Updated scene files to reflect related changes, likely to support new win condition mechanics.

--- Assets/Scripts/PlayerScripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerScripts/PlayerInventory.cs
@@ -1,44 +1,87 @@
 using UnityEngine;
-using UnityEngine.Tilemaps; 
+
+using UnityEngine.Tilemaps;
+
+
 
 public class PlayerInventory : MonoBehaviour
+
 {
+
     private bool hasKeycard = false;
 
-    public Tilemap mesasTilemap;
+
+
+    public Tilemap WinCondition;
+
     public TileBase mesaConDinero_Tile;
+
     public TileBase mesaVacia_Tile;
 
+
+
     void Update()
+
     {
+
         if (Input.GetKeyDown(KeyCode.E))
+
         {
+
             Interactuar();
+
         }
+
     }
+
+
 
     void Interactuar()
-    {
-        Vector3Int celdaActual = mesasTilemap.WorldToCell(transform.position);
 
-        TileBase tileEnCelda = mesasTilemap.GetTile(celdaActual);
+    {
+
+        Vector3Int celdaActual = WinCondition.WorldToCell(transform.position);
+
+
+
+        TileBase tileEnCelda = WinCondition.GetTile(celdaActual);
+
+
 
         if (tileEnCelda == mesaConDinero_Tile)
+
         {
-            mesasTilemap.SetTile(celdaActual, mesaVacia_Tile);
+
+            WinCondition.SetTile(celdaActual, mesaVacia_Tile);
+
+
 
             GameManager.Instance.RecogerDinero();
+
         }
+
     }
+
+
 
     public void CollectKeycard()
+
     {
+
         hasKeycard = true;
+
         Debug.Log("¡Keycard recogida!");
+
     }
 
+
+
     public bool HasKeycard()
+
     {
+
         return hasKeycard;
+
     }
+
 }
