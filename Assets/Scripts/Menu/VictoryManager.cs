10ed76327be8c49e73b4b41df60f2b3bd3d03578
Stikz <pucholj4@gmail.com>
2025-12-15 01:24:23 -0300

    levels 1 to 5, bug fixes.

--- Assets/Scripts/Menu/VictoryManager.cs
+++ Assets/Scripts/Menu/VictoryManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using static PauseManager;
 
 public class VictoryManager : MonoBehaviour
@@ -11,17 +12,29 @@ public class VictoryManager : MonoBehaviour
         victoryCanvas.SetActive(true);
         GameState.Paused = true;
         CursorMode.SetUI();
-        if (PlayerInput.all.Count > 0) PlayerInput.all[0].DeactivateInput();
+
+        if (PlayerInput.all.Count > 0)
+            PlayerInput.all[0].DeactivateInput();
     }
 
-    public void GoToLevelSelector()
+    public void GoToNextLevel()
     {
         GameState.Paused = false;
 
         if (PlayerInput.all.Count > 0)
             PlayerInput.all[0].ActivateInput();
 
-        ManageScenes.Instance.LoadScene("Main Menu");
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
 
     public void GoToMainMenu()
@@ -31,6 +44,6 @@ public class VictoryManager : MonoBehaviour
         if (PlayerInput.all.Count > 0)
             PlayerInput.all[0].ActivateInput();
 
-        ManageScenes.Instance.LoadScene("Main Menu");
+        SceneManager.LoadScene("Main Menu");
     }
 }
